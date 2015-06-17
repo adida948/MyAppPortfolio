2f1636078b1ade780ad620b21ebccf968a2c90a2
Danilo Carrion <adida948@hotmail.com>
2015-06-17 15:09:16 -0400

    Cleared up some code to make things less saturated. Button clicks are now being controlled by xml in activity_main.xml

--- app/src/main/java/com/danilocarrion/myappportfolio/MainActivity.java
+++ app/src/main/java/com/danilocarrion/myappportfolio/MainActivity.java
@@ -3,42 +3,17 @@ package com.danilocarrion.myappportfolio;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity  implements  View.OnClickListener{
-
-    Button streamButton;
-    Button scoresButton;
-    Button libraryButton;
-    Button biggerButton;
-    Button xyzButton;
-    Button capstoneButton;
+public class MainActivity extends Activity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Declaring the buttons
-        streamButton   = (Button) findViewById(R.id.streamer_button);
-        scoresButton   = (Button) findViewById(R.id.scores_button);
-        libraryButton  = (Button) findViewById(R.id.library_button);
-        biggerButton   = (Button) findViewById(R.id.bigger_button);
-        xyzButton      = (Button) findViewById(R.id.xyz_button);
-        capstoneButton = (Button) findViewById(R.id.capstone_button);
-
-        //Making each button show a toast every time they are pressed
-        streamButton.setOnClickListener(this);
-        scoresButton.setOnClickListener(this);
-        libraryButton.setOnClickListener(this);
-        biggerButton.setOnClickListener(this);
-        xyzButton.setOnClickListener(this);
-        capstoneButton.setOnClickListener(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,26 +22,9 @@ public class MainActivity extends Activity  implements  View.OnClickListener{
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View view) {
+    public void showToast(View view) {
         //For every button pressed, displayed a toast message
         Button b = (Button)view;
         Toast.makeText(MainActivity.this, "This button will launch " + b.getText().toString(), Toast.LENGTH_SHORT).show();
-
     }
 }
